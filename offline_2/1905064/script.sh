b69c8ffd2fc40671c577a6d4ea554c6cd58b8fc6
hyadess <sayemshahadsoummo@gmail.com>
2024-02-24 05:24:21 +0600

    offline-2 final

--- offline_2/1905064/script.sh
+++
@@ -1,4 +0,0 @@
-g++ -c *.cpp            # Compile all source files into object files
-g++ *.o -o prog   # Link the object files into the executable
-rm -r *.o
-./prog            # Run the compiled program
